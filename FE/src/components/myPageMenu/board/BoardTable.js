33436f9d0cede36e2d42a0d08cd5aaf013adf444
zhyeonn <156568917+zhyeonn@users.noreply.github.com>
2024-05-14 11:07:54 +0900

    Create BoardTable.js

---
+++ FE/src/components/myPageMenu/board/BoardTable.js
@@ -0,0 +1,25 @@
+import React from "react";
+import BoardTableItem from "./BoardTableItem";
+import "./Board.css";
+
+const BoardTable = (props) => {
+  return (
+    <div>
+      <table className="boardTable">
+        <thead>
+          <tr>
+            <td className="boardTdIndex btIndex">구분</td>
+            <td className="boardTdTitle btTitle">제목</td>
+            <td className="boardTdWriter btWriter">작성자</td>
+            <td className="boardTdDate btDate">날짜</td>
+          </tr>
+        </thead>
+      </table>
+      {props.board.map((title, index) => (
+        <BoardTableItem title={title} index={index} />
+      ))}
+    </div>
+  );
+};
+
+export default BoardTable;
