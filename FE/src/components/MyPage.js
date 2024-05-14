0bb7fd96c0a49d5b139cbe282edb5d8b0376c6e3
zhyeonn <156568917+zhyeonn@users.noreply.github.com>
2024-05-14 11:08:58 +0900

    Create MyPage.js

---
+++ FE/src/components/MyPage.js
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import "./MyPage.css";
+import NavbarWithoutUl from "./NavbarWithoutUl";
+import Notice from "./myPageMenu/notice/Notice";
+import Board from "./myPageMenu/board/Board";
+import ChangePassword from "./myPageMenu/changePassword/ChangePassword";
+import Calendar from "./calendar/Calendar";
+import Calculator from "./myPageMenu/calculator/Calculator";
+import Footer from "./Footer";
+import NoticeBoard from "./mainBoard/NoticeBoard";
+import NoticeContents from "./myPageMenu/notice/NoticeContents";
+import BoardContents from "./myPageMenu/board/BoardContents";
+
+const MyPage = () => {
+  const [changeComponent, setChangeComponent] = useState("");
+
+  return (
+    <div>
+      <NavbarWithoutUl />
+      <div>
+        <div>
+          <div className="pageTitle">
+            {changeComponent === "changePassword" && (
+              <div>비밀번호 변경하기</div>
+            )}
+            {changeComponent === "calendar" && <div>캘린더 보러가기</div>}
+            {changeComponent === "timeTable" && <div>시간표 보러가기</div>}
+            {changeComponent === "calculator" && <div>학점 계산기</div>}
+            {changeComponent === "notice" && <div>공지사항</div>}
+            {changeComponent === "noticeContents" && <div>공지사항</div>}
+            {changeComponent === "board" && <div>자유게시판</div>}
+            {changeComponent === "boardContents" && <div>자유게시판</div>}
+          </div>
+          <div>
+            <hr className="rowHr" />
+          </div>
+          <div className="pageContents">
+            <div className="myPageBoard">
+              <div className="myPage">
+                <div className="myPageBoardTitle">마이페이지</div>
+                <button
+                  className={
+                    changeComponent === "changePassword"
+                      ? "thisPage"
+                      : "myPageBoardMenu"
+                  }
+                  onClick={() => setChangeComponent("changePassword")}
+                >
+                  비밀번호 변경하기
+                </button>
+                <button
+                  className={
+                    changeComponent === "calendar"
+                      ? "thisPage"
+                      : "myPageBoardMenu"
+                  }
+                  onClick={() => setChangeComponent("calendar")}
+                >
+                  캘린더 보러가기
+                </button>
+                <button
+                  className={
+                    changeComponent === "timeTable"
+                      ? "thisPage"
+                      : "myPageBoardMenu"
+                  }
+                  onClick={() => setChangeComponent("timeTable")}
+                >
+                  시간표 보러가기
+                </button>
+                <button
+                  className={
+                    changeComponent === "calculator"
+                      ? "thisPage"
+                      : "myPageBoardMenu"
+                  }
+                  onClick={() => setChangeComponent("calculator")}
+                >
+                  학점 계산기
+                </button>
+              </div>
+              <div className="board">
+                <div className="myPageBoardTitle">게시판</div>
+                <button
+                  className={
+                    changeComponent === "notice"
+                      ? "thisPage"
+                      : "myPageBoardMenu"
+                  }
+                  onClick={() => setChangeComponent("notice")}
+                >
+                  공지사항
+                </button>
+                <button
+                  className={
+                    changeComponent === "board" ? "thisPage" : "myPageBoardMenu"
+                  }
+                  onClick={() => setChangeComponent("board")}
+                >
+                  자유게시판
+                </button>
+              </div>
+            </div>
+            <div className="linePage">
+              <div className="line"></div>
+              <div className="myPageMenuWrap">
+                <div className="myPageMenu">
+                  {changeComponent === "changePassword" && <ChangePassword />}
+                  {changeComponent === "calendar" && <Calendar />}
+                  {/* {changeComponent === "timeTable" && <TimeTable />} */}
+                  {changeComponent === "calculator" && <Calculator />}
+                  {changeComponent === "notice" && <Notice />}
+                  {changeComponent === "noticeContents" && <NoticeContents />}
+                  {changeComponent === "board" && <Board />}
+                  {changeComponent === "boardContents" && <BoardContents />}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default MyPage;
