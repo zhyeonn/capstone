4e19f56f641e25459b7811bbfd936efb734b8434
zhyeonn <156568917+zhyeonn@users.noreply.github.com>
2024-05-16 00:09:23 +0900

    Update MyPage.js

--- FE/src/components/MyPage.js
+++ FE/src/components/MyPage.js
@@ -7,9 +7,9 @@ import ChangePassword from "./myPageMenu/changePassword/ChangePassword";
 import Calendar from "./calendar/Calendar";
 import Calculator from "./myPageMenu/calculator/Calculator";
 import Footer from "./Footer";
-import NoticeBoard from "./mainBoard/NoticeBoard";
 import NoticeContents from "./myPageMenu/notice/NoticeContents";
 import BoardContents from "./myPageMenu/board/BoardContents";
+import InfoChange from "./myPageMenu/infoChange/InfoChange";
 
 const MyPage = () => {
   const [changeComponent, setChangeComponent] = useState("");
@@ -23,6 +23,7 @@ const MyPage = () => {
             {changeComponent === "changePassword" && (
               <div>비밀번호 변경하기</div>
             )}
+            {changeComponent === "infoChange" && <div>회원 정보 수정</div>}
             {changeComponent === "calendar" && <div>캘린더 보러가기</div>}
             {changeComponent === "timeTable" && <div>시간표 보러가기</div>}
             {changeComponent === "calculator" && <div>학점 계산기</div>}
@@ -38,6 +39,16 @@ const MyPage = () => {
             <div className="myPageBoard">
               <div className="myPage">
                 <div className="myPageBoardTitle">마이페이지</div>
+                <button
+                  className={
+                    changeComponent === "infoChange"
+                      ? "thisPage"
+                      : "myPageBoardMenu"
+                  }
+                  onClick={() => setChangeComponent("infoChange")}
+                >
+                  회원 정보 수정
+                </button>
                 <button
                   className={
                     changeComponent === "changePassword"
@@ -105,6 +116,7 @@ const MyPage = () => {
               <div className="line"></div>
               <div className="myPageMenuWrap">
                 <div className="myPageMenu">
+                  {changeComponent === "infoChange" && <InfoChange />}
                   {changeComponent === "changePassword" && <ChangePassword />}
                   {changeComponent === "calendar" && <Calendar />}
                   {/* {changeComponent === "timeTable" && <TimeTable />} */}
