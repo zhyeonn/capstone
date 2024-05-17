84f0aca01d2a32bfe174beaa482c3c413f3273d7
zhyeonn <156568917+zhyeonn@users.noreply.github.com>
2024-05-18 01:58:50 +0900

    Update InfoChange.js

--- FE/src/components/myPageMenu/infoChange/InfoChange.js
+++ FE/src/components/myPageMenu/infoChange/InfoChange.js
@@ -3,28 +3,26 @@ import "./InfoChange.css";
 
 const InfoChange = () => {
   return (
-    <div>
-      <div className="infoChangeWrap">
-        <div className="infoChange">
-          <div className="infoChangeHeader">회원 정보 수정</div>
-          <div className="infoChangeContentsWrap">
-            <div className="infoChangeContents">
-              <label className="infoChangeLabel">이름</label>
-              <input className="infoChangeInput" />
-            </div>
-            <div className="infoChangeContents">
-              <label className="infoChangeLabel">전화번호</label>
-              <input className="infoChangeInput" />
-            </div>
-            <div className="infoChangeContents">
-              <label className="infoChangeLabel">이메일</label>
-              <input className="infoChangeInput" />
-            </div>
+    <div className="infoChangePageWrap">
+      <div className="infoChangePage">
+        <div className="infoChangeHeader">회원 정보 수정</div>
+        <div>
+          <div className="infoChangeInputWrap">
+            <label className="infoChangeLabel">이름</label>
+            <input className="infoChangeInput" />
           </div>
-          <div className="infoChangeButtonWrap">
-            <button className="infoChangeButton">수정하기</button>
-            <button className="infoChangeCancleButton">취소하기</button>
+          <div className="infoChangeInputWrap">
+            <label className="infoChangeLabel">전화번호</label>
+            <input className="infoChangeInput" />
           </div>
+          <div className="infoChangeInputWrap">
+            <label className="infoChangeLabel">이메일</label>
+            <input className="infoChangeInput" />
+          </div>
+        </div>
+        <div className="infoChangeBtnWrap">
+          <button className="infoChangeBtn">변경하기</button>
+          <button className="infoChangecancleBtn">취소하기</button>
         </div>
       </div>
     </div>
